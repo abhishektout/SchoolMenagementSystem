467e9b6f2e6752bb2977acecd3618cdb9a2d3fb5
abhishek sen <abhishek.tout@gmail.com>
2023-10-31 14:07:05 +0530

    third commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import studentRouter from './Routes/Student.Route.js'
 import adminRouter from './Routes/Admin.Route.js';
 import bodyParser from "body-parser";
-
+import accountInfoRouter from './Routes/AccountInfo.Route.js'
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +14,7 @@ mongoose.connect(url).then(result => {
     app.use(cors())
     app.use("/admin", adminRouter);
     app.use("/student", studentRouter);
+    app.use("/accountInfo",accountInfoRouter);
     app.listen(3036, () => {
         console.log("Mongoose/server connect")
     })
